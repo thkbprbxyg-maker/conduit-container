8776e1e2f069fb9fc71a31aaa582649f9af44f5f
Ibrahim <ibx1232@gmail.com>
2026-03-25 10:34:45 +0100

    add frontend clean (no submodule)

--- frontend/src/app/core/interceptors/api.interceptor.ts
+++ frontend/src/app/core/interceptors/api.interceptor.ts
@@ -1,4 +1,4 @@
-import { environment } from 'src/nvironments/environment';
+import { environment } from 'src/environments/environment';
 import { Injectable } from "@angular/core";
 import {
   HttpEvent,
